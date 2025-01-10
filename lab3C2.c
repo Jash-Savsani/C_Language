4d470bac6d90f9c931380b8d198f748f3597c7d3
Jash Savsani <savsanijash8@gmail.com>
2025-01-10 21:09:18 +0530

    Add files via upload

---
+++ lab3C2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+ int p = 1, q = 2, r = 3, s = 4, e;
+  s = q * p;
+ e = r+s; 
+ printf("%d %d", e, s);
+}
---
+++ lab3C3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int x = 2;
+	if(x == 2)/*Remove semicolon*/
+ 		printf("Yes");
+	else
+ 		printf("No");
+}
