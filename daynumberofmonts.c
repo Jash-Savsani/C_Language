38fb27be7bf8c1ff69408d704bf36e242a9de19f
Jash Savsani <savsanijash8@gmail.com>
2025-04-19 13:24:03 +0530

    Add files via upload

---
+++ daynumberofmonts.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Enter a Month Number:");
+    scanf("%d",&a);
+    switch(a){
+        case 1:
+            printf("31 DAYS");
+            break;
+        case 2:
+            printf("28 DAYS");
+            break;
+        case 3:
+            printf("31 DAYS");
+            break;
+        case 4:
+            printf("30 DAYS");
+            break;
+        case 5:
+            printf("31 DAYS");
+            break;
+        case 6:
+            printf("30 DAYS");
+            break;
+        case 7:
+            printf("31 DAYS");
+            break;
+        case 8:
+            printf("31 DAYS");
+            break;
+        case 9:
+            printf("30 DAYS");
+            break;
+        case 10:
+            printf("31 DAYS");
+            break;
+        case 11:
+            printf("30 DAYS");
+            break;
+        case 12:
+            printf("31 DAYS");
+            break;
+        default:
+        printf("Invaid Month Number");
+    }
+}
