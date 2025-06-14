944bab2e6a237325cdfb66b8b7654e85318723fa
Jash Savsani <savsanijash8@gmail.com>
2025-06-14 20:45:09 +0530

    Add files via upload

---
+++ lab9b1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int i=65, n=90, i2=97, n2=122;
+	printf("Upper Case Alphabet:");
+	while(i<=n)
+	{
+		printf(" %c",i);
+		i++;	
+	}
+	printf("\nLower Case Alphbet:");
+	while(i2<=n2)
+	{
+		printf(" %c",i2);
+		i2++;	
+	}	
+}
