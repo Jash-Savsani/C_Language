a4ab44f176b0e076f771b1519a215ed59b059529
Jash Savsani <savsanijash8@gmail.com>
2025-07-10 20:40:08 +0530

    Add files via upload

--- lab12B1.c
+++ lab12B1.c
@@ -1,31 +1,15 @@
-/*Write a program to read the marks of one subject of 20 students and computes the number of students in categories 
-FAIL, PASS, FIRST CLASS and DISTINCTION using array.*/
 #include<stdio.h>
 void main()
 {
-	int n,i,f=0,p=0,fc=0,d=0;
-	printf("Enter Number of Students who Attempted Exam:");
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
+	int i,j,n,sum=0;
+	printf("Enter a Number:");
+	scanf("%d",&n);	
+	for(i=1;i<=n;i++)
 	{
-		printf("Enter %d Student marks in C : ",i+1);
-		scanf("%d",&a[i]);
-		if(a[i]<=25){
-			f++;
-		}
-		else if(a[i]<=50){
-			p++;
-		}
-		else if(a[i]<=75){
-			d++;
-		}
-		else if(a[i]<=100){
-			fc++;
+		for(j=1;j<=i;j++)
+		{
+			sum+=j;
 		}
 	}
-	printf("Total Number of Students in FAIL is: %d\n",f);
-	printf("Total Number of Students in PASS is: %d\n",p);
-	printf("Total Number of Students in DISTINCTION is: %d\n",d);
-	printf("Total Number of Students in FIRST CLASS is: %d\n",fc);
+	printf("Given Series Answer is : %d",sum);
 }
