473942b81286c4ab2b4196ad82b0b6af0581189f
Jash Savsani <savsanijash8@gmail.com>
2025-07-19 15:30:14 +0530

    Add files via upload

--- lab14A2.c
+++ lab14A2.c
@@ -1,26 +1,25 @@
-/*Given a N x M(N rows and M columns) matrix, print it upside down, i.e, last row should come first, 
-second last should come second......so on..and finally first row should come in last*/
 #include<stdio.h>
 void main()
 {
-	int i,j,r,c;
-	printf("Enter Number of Rows and Columns for 2D Array: ");
-	scanf("%d%d",&r,&c);
-	int a[r][c];
-	for(i=0;i<r;i++)
+	int a[10],p=0,n=0,z=0,i;
+	for(i=0;i<10;i++)
 	{
-		for(j=0;j<c;j++)
+		printf("Enter a Number:");
+		scanf("%d",&a[i]);
+		if(a[i]>0)
 		{
-			printf("Enter a[%d][%d]: ",i,j);
-			scanf("%d",&a[i][j]);
+			p++;
+		}
+		else if(a[i]<0)
+		{
+			n++;
+		}
+		else
+		{
+			z++;
 		}
 	}
-	printf("\n\n");
-	for(i=r-1;i>=0;i--)
-	{
-		for(j=0;j<c;j++)
-		{
-			printf("a[%d][%d] is : %d\n",i,j,a[i][j]);
-		}
-	}
+	printf("Total Positive Numbers:%d",p);
+	printf("\nTotal Negative Numbers:%d",n);
+	printf("\nTotal Zero Numbers:%d",z);
 }
