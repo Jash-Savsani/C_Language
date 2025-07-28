a705c0b871eea36489dc92341df430f309776dc8
Jash Savsani <savsanijash8@gmail.com>
2025-07-28 20:30:39 +0530

    Add files via upload

---
+++ lab16A1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,r,c;
+	printf("How many rows you want:");
+	scanf("%d",&r);
+	printf("How many colums you want:");
+	scanf("%d",&c);
+	int a[r][c];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("Enter a number a[%d][%d]=",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
