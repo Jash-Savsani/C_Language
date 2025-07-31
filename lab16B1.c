3bc26f0c7147e18bbfd889b0046b3cd17b9af8d6
Jash Savsani <savsanijash8@gmail.com>
2025-07-31 20:22:19 +0530

    Add files via upload

--- lab16B1.c
+++ lab16B1.c
@@ -1,31 +1,27 @@
-/*Delete all duplicate elements from an array using a pointer.*/
-#include<stdio.h>
+#include<stdio.h>/*haju aadhuro che*/
 void main()
 {
-	int i,n,j;
-	printf("Enter Number Of Elements in Array: ");
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
-	{
-		printf("Enter a[%d]: ",i);
-		scanf("%d",&a[i]); 
-	}
-	int *k,*l;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-		{
-			k=&a[i];
-			l=&a[j];
-			if((*k!=0)&&(*k==*l && i!=j))
-			{
-				*l=0;
-			}
-		}
-	}
-	for(i=0;i<n;i++)
-	{
-		printf("a[%d] is: %d\n",i,a[i]);
-	}
+    int r,c,i,j;
+    printf("Enter a row an arrary:");
+    scanf("%d",&r);
+    printf("Enter a column an arrary:");
+    scanf("%d",&c);
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("Enter a[i][j]:");
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d",a[i][j]);
+        }
+        printf("\n");
+    }
+    
 }
